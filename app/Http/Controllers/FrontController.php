04d4cee068542cab992ef23ffca8e8a2ba5a385c
Feri Adiansah <feriadiansah25@gmail.com>
2025-07-28 16:05:16 +0700

    sedikit penambahan

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\PricingRepositoryInterface;
+use App\Repositories\TransactionRepositoryInterface;
+use App\Services\MidtransService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontController extends Controller
 {
-    //
+    protected $midtransService;
+    protected $pricingRepository;
+    protected $transactionRepository;
+
+    public function __construct(MidtransService $midtransService, PricingRepositoryInterface $pricingRepository, TransactionRepositoryInterface $transactionRepository)
+    {
+        $this->pricingRepository = $pricingRepository;
+        $this->transactionRepository = $transactionRepository;
+        $this->midtransService = $midtransService;
+    }
+
+    public function index()
+    {
+        return view('front.index');
+    }
+
+    public function pricing()
+    {
+        $pricing_packages = $this->pricingRepository->getAll();
+        $user = Auth::user();
+        return view('front.pricing', compact('pricing_packages','user'));
+    }
 }
